43795d574454d5a6d22ef52a0701e8d231b1af6e
Islam Attrash <isattrash@gmail.com>
2018-03-06 22:49:08 +0200

    scoped style index.css per route

--- router.js
+++ router.js
@@ -1,7 +1,19 @@
 (() => {
 
+  const checkStatus = (cssResponse) => {
+    if (cssResponse.status >= 400) {
+      if (cssResponse.status !== 404) {
+        console.error(cssResponse);
+      }
+
+      throw new Error(cssResponse);
+    }
+  }
+
   const routerRootElementSelector = "#thin-router-content";
-  const loadedScriptSelector = "#thin-router-script"
+  const loadedScriptSelector = "#thin-router-script";
+  const loadedStyleSelector = "#thin-router-style";
+
   const routerChanged = async (newUrl) => {
     console.log(newUrl);
     const hash = newUrl.split("/#")[1];
@@ -12,33 +24,70 @@
     try {
       const routeHtmlFileUrl = `/views/${hash}/index.html`;
       const routeJsFileUrl = `/views/${hash}/index.js`;
+      const routeCssFileUrl = `/views/${hash}/index.css`;
       console.log(`Getting ${routeHtmlFileUrl}`);
-      console.log(`Getting ${routeJsFileUrl}`);
-      const response = await fetch(routeHtmlFileUrl);
-      const htmlContent = await response.text();
+      const htmlResponse = await fetch(routeHtmlFileUrl);
+      const htmlContent = await htmlResponse.text();
+
       const routerRootElement = document.querySelector(routerRootElementSelector);
       if(!routerRootElement) {
         throw new Error(`The router root DOM element with the selector ${routerRootElementSelector} doesn't exist`);
       }
 
       routerRootElement.innerHTML = htmlContent;
-      const isScriptLoaded = document.querySelector(loadedScriptSelector);
-      if(isScriptLoaded) {
-        isScriptLoaded.remove();
+
+      try {
+        const isScriptLoaded = document.querySelector(loadedScriptSelector);
+        if(isScriptLoaded) {
+          isScriptLoaded.remove();
+        }
+
+        console.log(`Getting ${routeJsFileUrl}`);
+        const jsResponse = await fetch(routeJsFileUrl);
+        checkStatus(jsResponse);
+        const jsContent = await jsResponse.text();
+
+        const thinRouterScript = document.createElement("script");
+        thinRouterScript.innerHTML = jsContent;
+        thinRouterScript.id = loadedScriptSelector.split("#")[1];
+        thinRouterScript.type = "text/babel";
+
+        if(!document.head) {
+          const headElement = document.createElement("head");
+          document.appendChild(headElement);
+        }
+  
+        document.head.appendChild(thinRouterScript);
+
+      } catch (e) {
+        
       }
 
-      const thinRouterScript = document.createElement("script");
-      thinRouterScript.src = routeJsFileUrl;
-      thinRouterScript.id = loadedScriptSelector.split("#")[1];
-      thinRouterScript.type = "text/babel";
+      try {
+        const isStyleLoaded = document.querySelector(loadedStyleSelector);
+        if(isStyleLoaded) {
+          isStyleLoaded.remove();
+        }
+        
+        console.log(`Getting ${routeCssFileUrl}`);
+        const cssResponse = await fetch(routeCssFileUrl);
+        checkStatus(cssResponse);
+        const cssContent = await cssResponse.text();
+
+        const thinRouterStyle = document.createElement("style");
+        thinRouterStyle.innerHTML = cssContent;
+        thinRouterStyle.id = loadedStyleSelector.split("#")[1];
+
+        if(!document.head) {
+          const headElement = document.createElement("head");
+          document.appendChild(headElement);
+        }
+  
+        document.head.appendChild(thinRouterStyle);
+      } catch (e) {
+        
+      }
       
-      if(!document.head) {
-        const headElement = document.createElement("head");
-        document.appendChild(headElement);
-      }
-
-      document.head.appendChild(thinRouterScript);
-      //@ts-ignore
       Babel.transformScriptTags(document.querySelectorAll(loadedScriptSelector));
 
     } catch(e) {
